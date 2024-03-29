b026b66915f998dcd4a19c1eb27e97218e7be42a
Caleb Sylvia <caleb_sylvia@yahoo.com>
2024-03-29 13:23:59 -0700

    lets try this again

--- src/app/Components/MapComponent.tsx
+++ src/app/Components/MapComponent.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react'
 import { APIProvider, Map } from '@vis.gl/react-google-maps'
 import { position } from '../Interfaces/Interfaces';
+
+
 let mapKey = process.env.NEXT_PUBLIC_MAP_KEY
 
 const MapComponent = ({lat, lng}: position) => {
